5b694f57a5a3c8e207203dc0374512a1c56ff559
AnnaB41 <159689092+AnnaB41@users.noreply.github.com>
2024-04-28 18:01:30 +0300

    Initial commit

---
+++ src/main/java/pages/SplashScreenPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class SplashScreen extends BasePage{
+
+    By textVersion = By.xpath("//*[@resource-id='com.telran.ilcarro:id/versionText']");
+
+    public String getTextVersion() {
+        return getTextBase(textVersion);
+    }
+
+
+}
